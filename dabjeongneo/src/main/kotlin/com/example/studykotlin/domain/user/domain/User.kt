79a99db7e4b2406a7f02c677fad47397298bc552
RyuJeaMin <kariel7673@gmail.com>
2025-01-05 17:00:28 +0900

    feat : User impl userDetails

--- dabjeongneo/src/main/kotlin/com/example/studykotlin/domain/user/domain/User.kt
+++ dabjeongneo/src/main/kotlin/com/example/studykotlin/domain/user/domain/User.kt
@@ -2,6 +2,10 @@ package com.example.studykotlin.domain.user.domain
 
 
 import com.example.studykotlin.domain.InterviewQuestion.domain.InterviewQuestion
+import com.example.studykotlin.domain.user.domain.type.Role
+import org.springframework.security.core.GrantedAuthority
+import org.springframework.security.core.authority.SimpleGrantedAuthority
+import org.springframework.security.core.userdetails.UserDetails
 import javax.persistence.*
 
 @Entity
@@ -10,10 +14,40 @@ class User(
     val id: Long? = null,
     val email: String,
     val password: String,
-    val role: String,
+    val role: Role,
     @OneToMany(mappedBy = "user") // mappedBy 쓴클래스가 조원(안쓴쪽이 주장) 조원은 조회밖에 못함.
     var interviewQuestions: MutableList<InterviewQuestion> = mutableListOf()
-    )
- {
+    ) : UserDetails{
 
- }
+
+    override fun getAuthorities(): MutableCollection<out GrantedAuthority> {
+        var authorities = mutableListOf<GrantedAuthority>()
+        authorities.add(SimpleGrantedAuthority(role.key))
+        return authorities
+    }
+
+    override fun getPassword(): String {
+        return password
+    }
+
+    override fun getUsername(): String {
+        return email
+    }
+
+    override fun isAccountNonExpired(): Boolean {
+        return true
+    }
+
+    override fun isAccountNonLocked(): Boolean {
+        return true
+    }
+
+    override fun isCredentialsNonExpired(): Boolean {
+        return true
+    }
+
+    override fun isEnabled(): Boolean {
+        return true
+    }
+
+}
