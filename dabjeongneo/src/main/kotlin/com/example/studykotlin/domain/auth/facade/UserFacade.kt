8ba94535cb19607bd0e30dcedc94335cfbfeb620
RyuJeaMin <kariel7673@gmail.com>
2025-01-10 00:14:22 +0900

    UserFacade : feat

---
+++ dabjeongneo/src/main/kotlin/com/example/studykotlin/domain/auth/facade/UserFacade.kt
@@ -0,0 +1,26 @@
+package com.example.studykotlin.domain.auth.facade
+
+import com.example.studykotlin.domain.auth.excpetion.SchoolIdAlreadyExistExcpetion
+import com.example.studykotlin.domain.user.domain.repository.UserRepostiory
+import org.springframework.stereotype.Service
+
+@Service
+class UserFacade(
+    val userRepostiory: UserRepostiory
+) {
+
+    fun checkByEmail(email: String){
+        if(userRepostiory.findByEmail(email) == null){
+            throw SchoolIdAlreadyExistExcpetion.EXCPETION
+        }
+    }
+
+    fun checkBySchoolId(schoolId: Int){
+        if(userRepostiory.findBySchoolId(schoolId) == null){
+            throw SchoolIdAlreadyExistExcpetion.EXCPETION
+        }
+    }
+
+
+
+}
