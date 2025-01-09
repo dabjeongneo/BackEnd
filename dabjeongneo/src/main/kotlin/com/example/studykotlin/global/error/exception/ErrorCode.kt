a345e77e0f5f110052edd3d67f961eb5b5eec2d1
RyuJeaMin <kariel7673@gmail.com>
2025-01-10 00:11:56 +0900

    errorCode에 enum : feat

--- dabjeongneo/src/main/kotlin/com/example/studykotlin/global/error/exception/ErrorCode.kt
+++ dabjeongneo/src/main/kotlin/com/example/studykotlin/global/error/exception/ErrorCode.kt
@@ -6,5 +6,8 @@ enum class ErrorCode(
 ) {
     USER_NOT_FOUND(404,"User not found"),
     INVALID_TOKEN(401,"Authorization token is Invalid or missing"),
-    EXPIRED_TOKEN(401,"Token expired")
+    EXPIRED_TOKEN(401,"Token expired"),
+    EAMIL_ALREADY_EXIST(409,"email already exists"),
+    SCHOOL_ID_ALREADY_EXIST(409,"school id already exists"),
+
 }
